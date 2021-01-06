5ed691cab42c6ce83781018c4a831cbf0ef1ee80
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-06 17:39:33 +0000

    third test passing

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -26,7 +26,10 @@ describe('Park', function() {
     assert.strictEqual(actual, 35);
   });
 
-  xit('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function(){
+    const actual = park.dinosaurs
+    assert.deepStrictEqual(actual, dinosaurs2021);
+  });
 
   xit('should be able to add a dinosaur to its collection');
 
