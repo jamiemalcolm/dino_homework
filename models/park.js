0d2619e184e59d382309c71f0c4beb0e4fa91826
jamiemalcolm <jmalcolm281@gmail.com>
2021-01-06 17:15:26 +0000

    initial commit, sudo code

---
+++ models/park.js
@@ -0,0 +1,17 @@
+// function to declare park 
+// name, ticket price, collection of dinos 
+
+
+// function to add dinosaur to its collection of dinosaurs
+
+// remove a dinosaur from its collection 
+
+// find the dino that attracts most visitors 
+
+// find all dinos of particular species 
+
+// calculate total number of visitors per day 
+
+// calculate total number of visitors per year 
+
+// calculate the totla revenue from ticket sales for a year 
